4ad1b4d7c3a8ef223aeac32a536b37c5a8dc17ab
userNameGithub <roshane.mhd@gmail.com>
2025-01-01 19:17:17 +0100

    ajt de verification de chaque elements

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,11 +5,11 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
-    new Route("/signup", "Iscription", "/pages/auth/signup.html"),
+    new Route("/signup", "Iscription", "/pages/auth/signup.html", "/js/auth/signup.js"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changer mon Mot de  passe", "/pages/auth/editPassword.html"),
+    new Route("/allResa", "Vos Réservations", "/pages/reservation/allResa.html"),
     new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
-
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
